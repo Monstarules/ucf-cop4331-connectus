899d60ecf3006d9487aa4a00a93f866872f30404
Brandon Rubio <brubio3724@gmail.com>
2021-09-03 19:01:50 -0400

    Fixed it for real this time

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -1,7 +1,6 @@
 <?php
 
-    // Stores json
-    // $data = getRequestInfo();
+    $data = getRequestInfo();
 
     $servername = "localhost";
     $username = "ConnectUs";
@@ -17,35 +16,34 @@
     else {
         $stmt = $conn->prepare("SELECT UserId,FirstName,LastName FROM Users WHERE UserName=? AND Password =?");
         $stmt->bind_param("ss", $data["login"], $data["password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-		if($row = $result->fetch_assoc()) {
-			returnWithInfo( $row['FirstName'], $row['LastName'], $row['UserId'] );
-		}
-		else {
-			// returnWithError("No Records Found");
+        if ($row = $result->fetch_assoc()) {
+            // returnWithInfo( $row['FirstName'], $row['LastName'], $row['UserId'] );
+        }
+        else {
+            // returnWithError("No Records Found");
             echo "No records found";
-		}
+        }
 
-		$stmt->close();
+        $stmt->close();
         $conn->close();
     }
 
-    // Should recieve json from the frontend
+    // API has not been configured to recieve JSON from the Frontend
     function getRequestInfo() {
-		//return json_decode(file_get_contents("login.json"), true);
-	}
+        return json_decode(file_get_contents("login.json"), true);
+    }
 
-    // I don't know what this does tbh
-    function sendResultInfoAsJson($obj) {
-		// header('Content-type: application/json');
-		// echo $obj;
+    // I dont know what this does
+    function sendResultInfoAsJson( $obj ) {
+        // header('Content-type: application/json');
+        // echo $obj;
         echo "<h1> YOU HAVE LOGGED IN </h1>";
-	}
-	
-    // ...................................
-	function returnWithInfo($firstName, $lastName, $id) {
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
+    }
+    
+    function returnWithInfo( $firstName, $lastName, $id ) {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
