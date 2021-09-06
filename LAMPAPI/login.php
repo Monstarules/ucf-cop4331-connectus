5c405acb75e92e6e916dd8833fa2dd3a663d1874
Brandon Rubio <brubio3724@gmail.com>
2021-09-06 13:56:50 -0400

    Fixed Login API
    API now reads input from a json and sends a json back as a result

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -7,6 +7,7 @@
     $password = "COP4331connectus";
     $database = "ConnectUs";
 
+    // Connect API to database
     $conn = new mysqli($servername, $username, $password, $database);
 
     if ($conn->connect_error) {
@@ -15,35 +16,40 @@
 
     else {
         $stmt = $conn->prepare("SELECT UserId,FirstName,LastName FROM Users WHERE UserName=? AND Password =?");
-        $stmt->bind_param("ss", $data["login"], $data["password"]);
+        $stmt->bind_param("ss", $data["UserName"], $data["Password"]);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($row = $result->fetch_assoc()) {
-            // returnWithInfo( $row['FirstName'], $row['LastName'], $row['UserId'] );
+            returnWithInfo($row['FirstName'], $row['LastName'], $row['UserId']);
         }
         else {
-            // returnWithError("No Records Found");
-            echo "No records found";
+            returnWithError("No Records Found");
         }
 
         $stmt->close();
         $conn->close();
     }
 
-    // API has not been configured to recieve JSON from the Frontend
+    // Retrieves the JSON with the login information
     function getRequestInfo() {
-        return json_decode(file_get_contents("login.json"), true);
+        return json_decode(file_get_contents('php://input'), true);
     }
 
-    // I dont know what this does
-    function sendResultInfoAsJson( $obj ) {
-        // header('Content-type: application/json');
-        // echo $obj;
-        echo "<h1> YOU HAVE LOGGED IN </h1>";
+    // Sends back a JSON object
+    function sendResultInfoAsJson($obj) {
+        header('Content-type: application/json');
+        echo $obj;
     }
-    
-    function returnWithInfo( $firstName, $lastName, $id ) {
+
+    // Sets return info with an error and calls sendResultInfoAsJson()
+    function returnWithError($err) {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
+    // Sets return info and calls sendResultInfoAsJson()
+    function returnWithInfo($firstName, $lastName, $id) {
         $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-        sendResultInfoAsJson( $retValue );
+        sendResultInfoAsJson($retValue);
     }
