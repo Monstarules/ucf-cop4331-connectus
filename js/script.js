01b028ffb7feaa825c97e76055dd8c42dfe985f5
Brandon Rubio <brubio3724@gmail.com>
2021-09-19 15:19:46 -0400

    Fixed some errors

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 // The small humble beginnings?
 // Eventually, the JS code will go here.
 
-var urlBase = 'http://connect-us.me';
+var urlBase = 'http://connect-us.me/LAMPAPI';
 var extension = 'php';
-
 
 var UserId = 0;
 var FirstName = "";
 var LastName = "";
-//updated?
 
 function doLogin()
 {
@@ -25,7 +23,7 @@ function doLogin()
 	var tmp = {UserName:UserName,Password:Password};
 	var jsonPayload = JSON.stringify( tmp );
 	
-	var url = urlBase + '/LAMPAPI/login.' + extension;
+	var url = urlBase + '/login.' + extension;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -53,7 +51,7 @@ function doLogin()
 				saveCookie();
 	
                 // DEFINITELY CHANGE THIS!!!!!
-				window.location.href = "contacts/index.html";
+				window.location.href = "contacts/contacts.html";
 			}
 		};
 		xhr.send(jsonPayload);
@@ -281,85 +279,85 @@ function searchContact()
 	}
 	
 }
-=======
-var userId = 0;
-var firstName = "";
-var lastName = "";
 
-function doLogin() {
-    userId = 0;
-    firstName = "";
-    lastName = "";
+// var userId = 0;
+// var firstName = "";
+// var lastName = "";
 
-    var login = document.getElementById("loginName").value;
-    var password = document.getElementById("loginPassword").value;
+// function doLogin() {
+//     userId = 0;
+//     firstName = "";
+//     lastName = "";
 
-    document.getElementById("loginResult").innerHTML = "";
+//     var login = document.getElementById("loginName").value;
+//     var password = document.getElementById("loginPassword").value;
 
-    var tmp = { login: login, password: password };
-    var jsonPayload = JSON.stringify(tmp);
+//     document.getElementById("loginResult").innerHTML = "";
 
-    var url = urlBase + '/Login.' + extension;
+//     var tmp = { login: login, password: password };
+//     var jsonPayload = JSON.stringify(tmp);
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var jsonObject = JSON.parse(xhr.responseText);
-                userId = jsonObject.id;
+//     var url = urlBase + '/login.' + extension;
 
-                if (userId < 1) {
-                    document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-                    return;
-                }
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", url, true);
+//     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+//     try {
+//         xhr.onreadystatechange = function () {
+//             if (this.readyState == 4 && this.status == 200) {
+//                 var jsonObject = JSON.parse(xhr.responseText);
+//                 userId = jsonObject.id;
 
-                firstName = jsonObject.firstName;
-                lastName = jsonObject.lastName;
+//                 if (userId < 1) {
+//                     document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+//                     return;
+//                 }
 
-                saveCookie();
+//                 firstName = jsonObject.firstName;
+//                 lastName = jsonObject.lastName;
 
-                window.location.href = "color.html";
-            }
-        };
-        xhr.send(jsonPayload);
-    }
-    catch (err) {
-        document.getElementById("loginResult").innerHTML = err.message;
-    }
+//                 saveCookie();
 
-}
+//                 window.location.href = "color.html";
+//             }
+//         };
+//         xhr.send(jsonPayload);
+//     }
+//     catch (err) {
+//         document.getElementById("loginResult").innerHTML = err.message;
+//     }
 
-function saveCookie() {
-    var minutes = 20;
-    var date = new Date();
-    date.setTime(date.getTime() + (minutes * 60 * 1000));
-    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-}
+// }
 
-function readCookie() {
-    userId = -1;
-    var data = document.cookie;
-    var splits = data.split(",");
-    for (var i = 0; i < splits.length; i++) {
-        var thisOne = splits[i].trim();
-        var tokens = thisOne.split("=");
-        if (tokens[0] == "firstName") {
-            firstName = tokens[1];
-        }
-        else if (tokens[0] == "lastName") {
-            lastName = tokens[1];
-        }
-        else if (tokens[0] == "userId") {
-            userId = parseInt(tokens[1].trim());
-        }
-    }
+// function saveCookie() {
+//     var minutes = 20;
+//     var date = new Date();
+//     date.setTime(date.getTime() + (minutes * 60 * 1000));
+//     document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+// }
 
-    if (userId < 0) {
-        window.location.href = "index.html";
-    }
-    else {
-        document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-    }
-}
+// function readCookie() {
+//     userId = -1;
+//     var data = document.cookie;
+//     var splits = data.split(",");
+//     for (var i = 0; i < splits.length; i++) {
+//         var thisOne = splits[i].trim();
+//         var tokens = thisOne.split("=");
+//         if (tokens[0] == "firstName") {
+//             firstName = tokens[1];
+//         }
+//         else if (tokens[0] == "lastName") {
+//             lastName = tokens[1];
+//         }
+//         else if (tokens[0] == "userId") {
+//             userId = parseInt(tokens[1].trim());
+//         }
+//     }
+
+//     if (userId < 0) {
+//         window.location.href = "index.html";
+//     }
+//     else {
+//         document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+//     }
+// }
